20dc9c24536cacce56e4275fd35c5fd3744d247c
sehbeom <tmxndjtm@likelion.org>
2019-06-11 22:28:23 +0900

    first commit of wordcount proj

---
+++ lvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\jsb\likelion\classlion\wordcount\wordcount\lvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
